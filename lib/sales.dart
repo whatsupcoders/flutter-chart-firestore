a335b1aff38ff256b7c8d4788dd24f4599c8b5d6
whatsupcoders <whatsupcoders@gmail.com>
2019-07-13 19:54:55 -0500

    clean up

--- lib/sales.dart
+++ lib/sales.dart
@@ -1,23 +1,16 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 class Sales {
   final int saleVal;
   final String saleYear;
   final String colorVal;
-  final DocumentReference reference;
-  Sales(this.saleVal,this.saleYear,this.colorVal,this.reference);
+  Sales(this.saleVal,this.saleYear,this.colorVal);
 
-  Sales.fromMap(Map<String, dynamic> map, {this.reference})
+  Sales.fromMap(Map<String, dynamic> map)
       : assert(map['saleVal'] != null),
         assert(map['saleYear'] != null),
         assert(map['colorVal'] != null),
         saleVal = map['saleVal'],
         colorVal = map['colorVal'],
         saleYear=map['saleYear'];
-
-
-  Sales.fromSnapshot(DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data, reference: snapshot.reference);
 
   @override
   String toString() => "Record<$saleVal:$saleYear:$colorVal>";
--- lib/task.dart
+++ lib/task.dart
@@ -1,23 +1,17 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Task {
   final int taskVal;
   final String taskDetails;
   final String colorVal;
-  final DocumentReference reference;
-  Task(this.taskDetails,this.taskVal,this.colorVal,this.reference);
+  Task(this.taskDetails,this.taskVal,this.colorVal);
 
-  Task.fromMap(Map<String, dynamic> map, {this.reference})
+  Task.fromMap(Map<String, dynamic> map)
       : assert(map['taskdetails'] != null),
         assert(map['taskVal'] != null),
         assert(map['colorVal'] != null),
         taskDetails = map['taskdetails'],
         taskVal = map['taskVal'],
         colorVal=map['colorVal'];
-
-
-  Task.fromSnapshot(DocumentSnapshot snapshot)
-      : this.fromMap(snapshot.data, reference: snapshot.reference);
 
   @override
   String toString() => "Record<$taskVal:$taskDetails>";
